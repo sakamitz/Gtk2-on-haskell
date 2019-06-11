9aae914acd67140e7c1c71a3145daae2dce8ac3a
sakamitz <sakamitz@yahoo.co.jp>
2019-06-11 22:20:57 +0800

    get & save

--- fortune-x/fortune-x.hs
+++ fortune-x/fortune-x.hs
@@ -1,15 +1,43 @@
+import System.IO
+import System.Process
+import System.Directory
 import Graphics.UI.Gtk
-import Graphics.UI.Gtk.Builder
 
+main :: IO ()
 main = do
     initGUI
     builder <- builderNew
     builderAddFromFile builder "fortune-x.glade"
+
     mainWindow <- builderGetObject builder castToWindow "mainWindow"
     textView <- builderGetObject builder castToTextView "textView"
-    buttonFortune <- builderGetObject builder castToButton "buttonFortune"
+    textBuffer <- builderGetObject builder castToTextBuffer "textBuffer"
+    buttonGet <- builderGetObject builder castToButton "buttonGet"
     buttonSave <- builderGetObject builder castToButton "buttonSave"
-    onClicked buttonFortune mainQuit
+
+    textViewSetBuffer textView textBuffer
+    onClicked buttonGet  $ getFortune textBuffer
+    onReleased buttonGet $ widgetSetSensitivity buttonSave True
+    onClicked buttonSave $ saveFortune textBuffer
     onDestroy mainWindow mainQuit
     widgetShowAll mainWindow
     mainGUI
+
+getFortune :: (TextBufferClass self) => self -> IO ()
+getFortune tbf = do
+    fortune <- readProcess "fortune" [] ""
+    textBufferSetText tbf fortune
+
+saveFortune :: (TextBufferClass self) => self -> IO ()
+saveFortune tbf = do
+    start <- textBufferGetStartIter tbf
+    end <- textBufferGetEndIter tbf
+    fortune <- textBufferGetText tbf start end False
+
+    existP <- doesDirectoryExist "fortunes"
+    _ <- if existP then return () else callCommand "mkdir fortunes"
+
+    nameEnd <- textBufferGetStartIter tbf
+    textIterForwardWordEnds nameEnd 3
+    filename <- textBufferGetText tbf start nameEnd False
+    writeFile ("fortunes/" ++ filename ++ "...") fortune
